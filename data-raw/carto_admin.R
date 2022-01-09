ddbd84c0db521c01c0ebc91f81ec3fa28b3bb0dd
mtmx <matai.data@gmail.com>
2022-01-09 11:45:42 +0100

    organisation en package

---
+++ data-raw/carto_admin.R
@@ -0,0 +1,3 @@
+## code to prepare `carto_admin` dataset goes here
+
+usethis::use_data(carto_admin, overwrite = TRUE)
---
+++ dev/dev.R
@@ -0,0 +1,4 @@
+library(usethis)
+use_mit_license("mtmx")
+
+usethis::use_readme_rmd( open = TRUE )
